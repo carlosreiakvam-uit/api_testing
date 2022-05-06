79e2c6e240738aa3d796a7985b0eb5e44904cc54
carlos reiakvam <cre032@uit.no>
2022-05-06 23:20:22 +0200

    Delete redundant commented code

--- src/App.js
+++ src/App.js
@@ -7,46 +7,24 @@ import './App.css';
 const PATH_BASE = "http://localhost:3000";
 const PATH_USERS = "/users";
 
-// Functional stateless component
-// destructured props
-// const Table = ({list, onDismiss}) =>
-//     <div>
-//         {list.map(item =>
-//             <div key={item.phone}>
-//                 <span>{item.first_name} </span>
-//                 <span>{item.last_name} </span>
-//                 <span>{item.phone} </span>
-//                 <span>{item.address} </span>
-//                 <span>{item.postal_code} </span>
-//                 <Button onClick={() => onDismiss(item.phone)}>
-//                     Dismiss
-//                 </Button>
-//             </div>)}
-//     </div>
+function Button(props) {
+    const {
+        onClick,
+        className = '', //  default '' because optional
+        children
+    } = props
+    return (
+        <button
+            onClick={onClick}
+            className={className}
+            type="button"
+        >
+            {children}
+        </button>
+    );
+}
 
 
-// // Functional stateless component
-// function Button(props) {
-//     const {
-//         onClick,
-//         className = '', //  default '' because optional
-//         children
-//     } = props
-//     return (
-//         <button
-//             onClick={onClick}
-//             className={className}
-//             type="button"
-//         >
-//             {children}
-//         </button>
-//     );
-// }
-
-
-// console.log("render: state", this.state)
-// console.log("render: state.result", this.state.result)
-// console.log("render: type of state.result", typeof this.state.result)
 function App() {
     const [users, setUsers] = useState([])
 
@@ -147,27 +125,7 @@ function App() {
     )
 }
 
-// Tidligere constructor
-// constructor(props) {
-//     super(props);
-//     this.state = {
-//         result: [],
-//         searchTerm: `${PATH_BASE}${PATH_USERS}`,
-//     }
-//
-//     // bindings
-//     this.setUsers = this.setUsers.bind(this);
-//     this.onDismiss = this.onDismiss.bind(this);
-//
-//
-// }
 
-// function setUsers(result)
-// {
-//     this.setState({result});
-//     console.log("setUsers: result", result)
-// }
-//
 // function onDismiss(phone)
 // {
 //     console.log("onDismiss: phone: ", phone)
@@ -183,18 +141,6 @@ function App() {
 //
 //     console.log("onDismiss: this.state.result etter filter", this.state.result)
 // }
-//
-// function onSort(item)
-// {
-//
-// }
-//
-// function componentDidMount()
-// {
-//     fetch(`${PATH_BASE}${PATH_USERS}`)
-//         .then(response => response.json())
-//         .then(result => this.setUsers(result))
-//         .catch(error => error);
-// }
+
 
 export default App;
